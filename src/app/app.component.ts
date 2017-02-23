857fc8dac17894877f82fbfd13063d81287c54c2
Admin <admin@MacMini.local>
2017-02-23 16:10:13 +0100

    create Hike class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hike } from './shared/hike';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'an hicking app !';
+  hikes: Hike;
+
 }
